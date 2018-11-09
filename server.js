5ff95b1c02da9e36971fd909bbc7036b3564157d
Simon Anderson <simon.anderson111@gmail.com>
2018-11-09 15:21:54 +1100

    new message

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('From Puppet Pipelines. Your Node.js application is working!\n\n\n');
+    response.end('This is a new message. Your Node.js application is working!\n\n\n');
 }).listen(80);
 
 console.log('Server started');
