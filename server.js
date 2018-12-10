a19dcc720ee43311b5b0116ac54b7f7766bbd439
boltKrank <simon.anderson111@gmail.com>
2018-12-10 12:58:31 +1100

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('This is a new me ssage!\n\n\n');
+    response.end('I hope this works!\n\n\n');
 }).listen(80);
 
 console.log('Server started');
