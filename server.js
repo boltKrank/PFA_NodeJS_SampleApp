c2caadc221c49b0c0b2ba746bb4ddb500b65618b
boltKrank <simon.anderson111@gmail.com>
2019-04-25 00:28:12 +1000

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('All done!\n\n\n');
+    response.end('New Message!\n\n\n');
 }).listen(80);
 
 console.log('Server started');
