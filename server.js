8d0e412541e5a498df02488e0e23a80b32228367
boltKrank <simon.anderson111@gmail.com>
2019-04-25 00:13:05 +1000

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('できました!\n\n\n');
+    response.end('All done!\n\n\n');
 }).listen(80);
 
 console.log('Server started');
