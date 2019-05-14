286ec9a0b370a7960b6bea8f793ee9074b602fb8
boltKrank <simon.anderson111@gmail.com>
2019-05-14 17:09:12 +1000

    Update test.js

--- test.js
+++ test.js
@@ -3,9 +3,10 @@ const {Builder, By, Key, until} = require('selenium-webdriver');
 (async function example() {
   let driver = await new Builder().forBrowser('chrome').build();
   try {
+    await driver.add_argument('headless')
     await driver.get('http://localhost');
     await driver.findElement(By.name('q'));.sendKeys('webdriver', Key.RETURN);
-    await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+    await driver.getPageSource().contains("New Message!")
   } finally {
     await driver.quit();
   }
